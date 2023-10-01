a7c01fdcda7739dd610e82bab6581fc3566ffc5b
Vbrawl <greninjagr@gmail.com>
2023-10-01 20:14:27 +0300

    Added load protection for the drag-n-drop module.

--- main.php
+++ main.php
@@ -7,9 +7,14 @@ namespace DRAG_N_DROP {
 
     require_once(MULTI_DEVICE_SUPPORT_PATH.'/main.php');
 
+    $LOADED = false;
+
     function load_draggable() {
-        \MULTI_DEVICE_SUPPORT\enable_all();
-        load_file(DRAG_N_DROP_RESOURCES_PATH.'/draggable.css');
-        load_file(DRAG_N_DROP_RESOURCES_PATH.'/draggable.js', 'defer');
+        if(!$LOADED) {
+            \MULTI_DEVICE_SUPPORT\enable_all();
+            load_file(DRAG_N_DROP_RESOURCES_PATH.'/draggable.css');
+            load_file(DRAG_N_DROP_RESOURCES_PATH.'/draggable.js', 'defer');
+            $LOADED = true;
+        }
     }
 }
